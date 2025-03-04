3a6866dcd4cfe2eda9d3e237024db7d0a6167974
18423375893 <3168691636@qq.com>
2025-03-04 14:35:00 +0800

    11111

---
+++ 简约TAB/0-1.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.s_button')[1].onclick = ()=>{
+  let lists = document.querySelectorAll('.item');
+  document.querySelector("#slide").appendChild(lists[0]);
+}
+
+
+document.querySelectorAll('.s_button')[0].onclick = () => {
+  let lists = document.querySelectorAll('.item');
+  document.querySelector("#slide").prepend(lists[lists.length-1]);
+}
+
+
+// function handleSee() {
+//   console.log("See");
+//  }
+
